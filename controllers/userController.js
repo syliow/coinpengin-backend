e27a18f5f6a4bdc6e69c39e6257476dc2a7a80bc
Shanyi Liow <shanyiliow1107@gmail.com>
2025-05-06 13:11:12 +0800

    chore: add coingecko api key

--- controllers/userController.js
+++ controllers/userController.js
@@ -76,7 +76,12 @@ const getUser = async (req, res) => {
     let coinData = [];
     try {
       const response = await axios.get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
+        {
+          headers: {
+            "x-cg-api-key": process.env.COINGECKO_API_KEY,
+          },
+        }
       );
       coinData = response.data;
     } catch (axiosError) {
