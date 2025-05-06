b4f81fcc249a3407e66e85007005930ed30f21aa
Shanyi Liow <shanyiliow1107@gmail.com>
2025-05-06 18:06:16 +0800

    chore: debug api key issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,9 +74,11 @@ const getUser = async (req, res) => {
       req.user.id
     );
     let coinData = [];
+    const coingeckoApiKey = process.env.COINGECKO_API_KEY;
+    console.log(coingeckoApiKey);
     try {
       const response = await axios.get(
-       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&x_cg_demo_api_key=${process.env.COINGECKO_API_KEY}`
+       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&x_cg_demo_api_key=${coingeckoApiKey}`
       );
       coinData = response.data;
     } catch (axiosError) {
